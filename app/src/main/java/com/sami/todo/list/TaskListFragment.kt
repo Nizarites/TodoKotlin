6ce5e3aa16af5cdc8294b4d17872a781b20e6d8e
skanaie-atrian <sami.kanaieatrian@ensiie.fr>
2024-11-28 15:14:34 +0100

    TaskListAdapter: Utilisation

--- app/src/main/java/com/sami/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/sami/todo/list/TaskListFragment.kt
@@ -9,12 +9,14 @@ import com.sami.todo.R
 
 class TaskListFragment : Fragment() {
     private var taskList = listOf("Task 1", "Task 2", "Task 3")
+    private val adapter = TaskListAdapter()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container,false)
+        adapter.currentList = taskList
         return rootView
     }
 }
