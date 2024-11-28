e30ecdc044b3493d3cd94d08abca568b3f2fae5b
skanaie-atrian <sami.kanaieatrian@ensiie.fr>
2024-11-28 16:48:01 +0100

    ADD BUTTON

--- app/src/main/java/com/sami/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/sami/todo/list/TaskListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.sami.todo.R
+import java.util.UUID
 
 class TaskListFragment : Fragment() {
     private var taskList = listOf(
@@ -21,12 +24,22 @@ class TaskListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_task_list, container,false)
-        adapter.currentList = taskList
+        adapter.submitList(taskList)
         return rootView
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // on utilise `requireView()` pour éviter les erreurs de `null`
         val recyclerView = requireView().findViewById<RecyclerView>(R.id.task_list)
         recyclerView.adapter = adapter
+
+        val addButton = requireView().findViewById<FloatingActionButton>(R.id.add_task_button)
+        addButton.setOnClickListener {
+            val newTask =
+                Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList = taskList + newTask
+            adapter.submitList(taskList)
+
+        }
     }
+
 }
