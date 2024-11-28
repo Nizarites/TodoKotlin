7613fc4b4b0cdc5abe3058cbfa6bf9e7876108df
skanaie-atrian <sami.kanaieatrian@ensiie.fr>
2024-11-28 15:09:44 +0100

    Create TaskListFragment

---
+++ app/src/main/java/com/sami/todo/list/TaskListFragment.kt
@@ -0,0 +1,20 @@
+package com.sami.todo.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sami.todo.R
+
+class TaskListFragment : Fragment() {
+    private var taskList = listOf("Task 1", "Task 2", "Task 3")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+        return rootView
+    }
+}
